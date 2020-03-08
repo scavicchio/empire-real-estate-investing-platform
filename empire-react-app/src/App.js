9fc77dd9b2683a38ae5d970b89d4ebf7d7bb2243
Mohamad Sherif <mjs1342@gmail.com>
2020-03-07 19:39:45 -0500

    investor homepage - links not connected

--- empire-react-app/src/App.js
+++ empire-react-app/src/App.js
@@ -1,47 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
-
-import { Alert } from "shards-react";
+import React, { Component } from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "shards-ui/dist/css/shards.min.css"
-import './App.css';import { Card, CardBody, CardTitle, CardSubtitle } from "shards-react";
+import Header from './Header/Header';
+import { Button } from "shards-react";
+import { Container, Row, Col } from "shards-react";
+import { Card, CardBody } from "shards-react";
+import { Link, animateScroll as scroll } from "react-scroll";
+import { StickyContainer, Sticky } from 'react-sticky';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          testing reaction
-          
-        </p>
-        <div className="example">
-        {/* <Button theme="success">Success</Button> */}
-        </div>
-        
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends Component{
+
+  render(){
+    return (
+      <div className="App">
+          <Header />
+
+          <div className="MainSection" style={{height:450}}>
+            <h1 style={{marginTop: 130, marginLeft: 50}}>Democratizing Commercial </h1>
+            <h1 style={{marginLeft: 50}}>Real Estate Investment</h1>
+
+            <Link activeClass="active" className="test1" to="second" spy={true} smooth={true} duration={500} >
+              <Button theme="secondary" style={{right:0, position:"absolute", marginRight: 150, marginTop:100}}>
+                Learn More!
+              </Button>
+            </Link>
+
+            <Button theme="dark" style={{right:0, position:"absolute", marginRight: 300, marginTop:100}}>
+                Get Started
+              </Button>
+            
+          </div>
+
+          <div className="second" style={{paddingTop: 25, height:650, backgroundColor:'grey'}}>
+            <h2 style={{marginTop: 150, marginLeft: 50}}>Whats Empire?</h2>
+            <p style={{marginLeft: 50, maxWidth:500}}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+             nisi ut aliquip ex ea commodo consequat. 
+            </p>
+          </div>
+
+          <div style={{paddingTop: 25, height:650, backgroundColor:'yellow'}}>
+            <h2 style={{marginTop: 150, marginLeft: 50}}>Whats Commercial Real Estate?</h2>
+            <p style={{marginLeft: 50, maxWidth:500}}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+             Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+             nisi ut aliquip ex ea commodo consequat. 
+            </p>
+          </div>
+
+          <div style={{paddingTop: 25, height:650}}>
+            <h2 style={{marginTop: 50, marginLeft: 50, marginBottom:50}}>How our platform is better</h2>
+            <Container className="dr-example-container">
+              <Row>
+                <Col>
+                  <Card>
+                  <CardBody>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                      nisi ut aliquip ex ea commodo consequat. </p>
+                  </CardBody>
+                  </Card>
+                </Col>
+
+                <Col>
+                  <Card>
+                  <CardBody>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                      nisi ut aliquip ex ea commodo consequat. </p>
+                  </CardBody>
+                  </Card>
+                </Col>
+
+                <Col>
+                  <Card>
+                  <CardBody>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                      nisi ut aliquip ex ea commodo consequat. </p>
+                  </CardBody>
+                  </Card>
+                </Col>
+              </Row>
+
+            </Container>
+          </div>
+      </div>
+      
+    );
+  }
 }
 
-function CardBodyTitleSubtitleExample() {
-  return (
-    <Card>
-      <CardBody>
-        <CardTitle>Card Title</CardTitle>
-        <CardSubtitle>Card subtitle</CardSubtitle>
-        Nunc quis nisl ac justo elementum sagittis in quis justo.
-      </CardBody>
-    </Card>
-  );
-}
-
-export default CardBodyTitleSubtitleExample;
+export default App;
